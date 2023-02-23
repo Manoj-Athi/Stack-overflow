dc027eb202e5bd98b37e24888c0696f59b7e1db5
Manoj-Athi <manojathi.13@gmail.com>
2023-02-23 14:39:04 +0530

    testing

---
+++ server/connectMongoDb.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect( process.env.CONNECTION_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log(`MongoDB connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 import express from 'express'
-import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
 import userRoutes from './routes/users.js'
 import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
+import connectDB from './connectMongoDb.js'
 
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
 app.use(express.json({limit: "30mb", extended: true}))
 app.use(express.urlencoded({limit: "30mb", extended: true}))
 app.use(cors());
@@ -22,11 +23,5 @@ app.use('/questions', questionRoutes)
 app.use('/answer', answerRoutes)
 
 const PORT = process.env.PORT || 5000
-
-const DATABASE_URL = process.env.CONNECTION_URL
-
-// mongoose.connect( DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-//     .then(() => app.listen(PORT, () => {console.log(`server running on port ${PORT}`)}))
-//     .catch((err) => console.log(err.message))
 
 app.listen(PORT, () => {console.log(`server running on port ${PORT}`)})
